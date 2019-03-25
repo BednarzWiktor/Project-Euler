1cd67957b1b3b386263f291183866da8037e4bc2
Wiktor Bednarz <b.w.roboczy@gmail.com>
2019-03-25 11:35:38 +0100

    submit problem 7

---
+++ 7-10001st_prime.js
@@ -0,0 +1,27 @@
+// What is the 10 001st prime number?
+'use strict'
+
+const limit = 10001;
+
+// Test if number is prime
+const isPrime = (num) => {
+  for (let i=2; i<num; i++) {
+    if (num%i===0) {
+        return false;
+    }
+  }
+  return true;
+}
+
+// return nth prime: count numbers up to provided limit
+const main = (limit) => {
+  let counter=0,
+      i=0;
+  while (counter<=limit) {
+    i++;
+    isPrime(i) ? counter++ : null;
+  }
+  return i
+}
+
+console.log(main(limit));
